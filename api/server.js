01872aa2c0072952ef52dc15f0f569f903398397
Marc Thiongo <marcthuo@gmail.com>
2019-04-04 13:02:01 -0400

    deploy

--- api/server.js
+++ api/server.js
@@ -11,7 +11,8 @@ server.use(express.json());
 server.get('/', async (req, res) => {
   try {
     const shoutouts = await db('shoutouts');
-    res.status(200).json(shoutouts);
+    const messageOfTheDay = process.env.MOTD || 'Hello World'
+    res.status(200).json({motd: messageOfTheDay, shoutouts});
   } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
@@ -22,8 +23,8 @@ server.post('/', async (req, res) => {
   try {
     const [id] = await db('shoutouts').insert(req.body);
     const shoutouts = await db('shoutouts');
-
-    res.status(201).json(shoutouts);
+    const messageOfTheDay = process.env.MOTD || 'Hello World'
+    res.status(201).json({motd: messageOfTheDay, shoutouts});
   } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot add the shoutout' });
